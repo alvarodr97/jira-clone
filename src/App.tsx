63027cb96486364d2707151728605054cd614275
Alvaro <alvarodr_97@hotmail.com>
2024-09-18 21:53:35 +0200

    fix overflow

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Navigation } from "./components/navigation/Navigation";
 
 function App() {
   return (
-    <div className="w-full h-full flex flex-row">
+    <div className="w-screen h-screen flex flex-row">
       <Toaster richColors duration={3000} position="top-right" />
       <Navigation />
       <div className="flex-1 min-w-0 z-0 overflow-auto">
