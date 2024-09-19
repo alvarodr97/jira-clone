e8570ea5c1c592fb2524200c77ff14a2e695b5f2
Alvaro <alvarodr_97@hotmail.com>
2024-09-19 20:26:32 +0200

    feat: projectLayout

---
+++ src/pages/ProjectLayout.tsx
@@ -0,0 +1,24 @@
+import useBoundStore from "@/store/store";
+import { Breadcrumbs } from "@/components/Breadcrumbs";
+
+interface Props {
+  pageTitle: string;
+  children: JSX.Element;
+}
+
+export const ProjectLayout = ({ pageTitle, children }: Props) => {
+  const projectName = useBoundStore((state) => state.projectName);
+  const breadcrumbs: string[] = ["Projects", projectName, pageTitle];
+
+  return (
+    <div className="flex flex-col w-full h-full py-8 pl-8 pr-6">
+      <nav aria-label="Breadcrumbs">
+        <Breadcrumbs breadcrumbs={breadcrumbs} />
+      </nav>
+      <header className="mt-3 mb-6">
+        <h1 className="text-2xl font-medium">{pageTitle}</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+};
--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,20 +1,10 @@
-import { Breadcrumbs } from "@/components/Breadcrumbs";
+import { ProjectLayout } from "../ProjectLayout";
 import { FormSettings } from "./FormSettings";
-import useBoundStore from "@/store/store";
 
 export const Settings = () => {
-  const projectName = useBoundStore((state) => state.projectName);
-  const breadcrumbs: string[] = ["Projects", projectName, "Settings"];
-
   return (
-    <div className="flex flex-col w-full h-full py-8 pl-8 pr-6">
-      <Breadcrumbs breadcrumbs={breadcrumbs} />
-      <header className="flex justify-between mt-3 mb-6 text-2xl font-medium">
-        Settings
-      </header>
-      <div>
-        <FormSettings />
-      </div>
-    </div>
+    <ProjectLayout pageTitle="Settings">
+      <FormSettings />
+    </ProjectLayout>
   );
 };
