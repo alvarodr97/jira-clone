8760648de4bcd88ac751a90aacc9221fed66243d
Alvaro <alvarodr_97@hotmail.com>
2025-01-02 19:21:44 +0100

    fix: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: [
@@ -48,5 +50,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
